f607d4b3070ef60f5995a3e8b186da88972bf6de
iara.zas <iarazasfernandez15@gmail.com>
2024-10-15 19:36:14 +0200

    operaciones

---
+++ src/Operaciones.kt
@@ -0,0 +1,14 @@
+
+
+fun main() {
+    val a=5
+    val b=10
+
+    println("suma es:" + (a+b))
+    println("resta es:" + (a-b))
+    println("multi es:" + (a*b))
+    println("division es:" + (a/b))
+    println("resto es:" + (a%b))
+
+
+}
