bc002a3c60af29c35ad9aac109689dd9b7dbb539
iara.zas <123583066+iarazuska@users.noreply.github.com>
2024-11-19 19:56:10 +0100

    comentarios

--- src/ejer2/Almacen.kt
+++ src/ejer2/Almacen.kt
@@ -4,7 +4,9 @@ class Almacen {
     private val estanteria = Array(5) { Array<Bebidas?>(5) { null } }
     private val bebidas = mutableListOf<Bebidas>()
 
+    //agregamos bebidas
     fun agregarBebida(bebida: Bebidas) {
+        //ver si existe este id
         var yaExiste=false
         for(b in bebidas){
             if(b.id==bebida.id){
@@ -15,10 +17,12 @@ class Almacen {
         if (yaExiste){
             print("ya exixte")
         }else{
+            //ver si hay sitio
             var sitio=false
             for(i in 0 until 5){
                 for(j in 0 until 5){
                     if (estanteria[i][j]==null){
+                        //si hay sitio lo colocamos
                         estanteria[i][j]=bebida
                         bebidas.add(bebida)
                         println("bebida colocada en [$i][$j]")
@@ -34,6 +38,7 @@ class Almacen {
         }
     }
 
+    //eliminar bebida por id
     fun eliminarBebida(id: Int) {
         var tenemosBebida: Bebidas? = null
         // buscamos la bebida
@@ -62,7 +67,7 @@ class Almacen {
         }
     }
 
-
+    //ver que bebidas hay
     fun verBebidas() {
         println("lista")
         for (bebida in bebidas) {
@@ -75,11 +80,12 @@ class Almacen {
         }
     }
 
+    //precio de total
     fun precioTotal(): Double {
         var total = 0.0
         for (bebida in bebidas) {
             if (bebida is BebidasAzucaradas && bebida.promocion) {
-                // Si es una bebida azucarada en promoción, aplicamos 10% de descuento
+                // si esta en promocion restamos el descuento
                 total += bebida.precio * 0.9
             } else {
                 total += bebida.precio
@@ -88,12 +94,13 @@ class Almacen {
         return total
     }
 
+    //precio por marca
     fun precioMarca(marca: String): Double {
         var total = 0.0
         for (bebida in bebidas) {
             if (bebida.marca == marca) {
                 if (bebida is BebidasAzucaradas && bebida.promocion) {
-                    // Si es una bebida azucarada en promoción, aplicamos 10% de descuento
+                    // si esta en promocion restamos el descuento
                     total += bebida.precio * 0.9
                 } else {
                     total += bebida.precio
@@ -103,13 +110,14 @@ class Almacen {
         return total
     }
 
+    //precio por columna
     fun precioColumna(columna: Int): Double {
         var total = 0.0
         for (i in 0 until 5) {
             val bebida = estanteria[i][columna]
             if (bebida != null) {
                 if (bebida is BebidasAzucaradas && bebida.promocion) {
-                    // Si es una bebida azucarada en promoción, aplicamos 10% de descuento
+                    // si esta en promocion restamos el descuento
                     total += bebida.precio * 0.9
                 } else {
                     total += bebida.precio
